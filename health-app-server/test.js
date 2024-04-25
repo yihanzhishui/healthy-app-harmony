af2b051bede09b23abd03e9ee5945449a27bdb6c
一寒止水 <321840565@qq.com>
2024-04-25 15:02:24 +0800

    modified:   health-app-server/package-lock.json 	deleted:    health-app-server/router/verify_code.js 	renamed:    health-app-server/router/user.js -> health-app-server/router_handler/diet_handler.js 	modified:   health-app-server/test.js 	modified:   "\343\200\214\345\201\245\345\272\267\346\226\271\350\210\237\343\200\215\346\216\245\345\217\243\346\226\207\346\241\243.md"

--- health-app-server/test.js
+++ health-app-server/test.js
@@ -8,21 +8,37 @@ const salt = require('./utils/salt')
 const app = express()
 
 app.use(bodyParser.json())
-
-// 生成 JWT 的中间件
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(expressJwt({ secret: salt }).unless({ path: ['/login'] }))
 
 // 登录接口
 app.post('/login', (req, res) => {
-    // 假设用户名为 "admin"，密码为 "password"
+    // 假设用户名为 "admin"，密码为 "123456"
     const { username, password } = req.body
     console.log(username, password)
-    if (username === 'admin' && password === 'password') {
+    if (username === 'admin' && password === '123456') {
         // 生成 JWT
         const token = jwt.sign({ username }, salt, { expiresIn: '1h' })
         res.json({ token: 'Bearer ' + token })
     } else {
         res.status(401).json({ error: 'Invalid username or password' })
     }
+})
+
+// 处理token解析失败
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({ error: '无效 token' })
+    }
+})
+
+app.post('/get', (req, res) => {
+    console.log(req.body)
+    res.send({
+        status: 'success',
+        code: 200,
+        data: req.user,
+    })
 })
 
 // 监听 3000 端口
